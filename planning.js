2a49465cf0060bb9df7e8ca762673f8d9cf3f0ee
zoelittleada <zoe.little@ada.ac.uk>
2024-11-01 14:09:57 +0000

    comments

--- planning.js
+++ planning.js
@@ -24,7 +24,7 @@ const airportsData = readCsv('airports.csv');
 const aeroplaneData = readCsv('aeroplanes.csv');
 const valid_flight_data = readCsv('valid_flight_data.csv');
 const invalid_flight_data = readCsv('invalid_flight_data.csv');
-const allFlightData = valid_flight_data.concat(invalid_flight_data); //concatenates both csvs
+const allFlightData = valid_flight_data.concat(invalid_flight_data); //concatenates both csvs 
 
 
 function details(airportsData, aeroplanesData, allFlightData) {
@@ -41,15 +41,15 @@ function details(airportsData, aeroplanesData, allFlightData) {
 
     let flightError = ""; //makes sure error message only pushed once for each flight
 
-    // Handle invalid airport code
+    // finds match between index 0 in airports and arrival airport in filght data
     const foundAirport = airportsData.find(airport => airport[0] === arrivalAirport);
-    if (!foundAirport) {
+    if (!foundAirport) { // executes if match is found 
       flightError = `The arrival airport code for this flight is invalid (${arrivalAirport})`;
     }
 
-    // Find matching aeroplane type
+    // Find match between index 0 in aeroplane data and aircraft type
     const plane = aeroplanesData.find(plane => plane[0] === aircraftType);
-    if (plane) {
+    if (plane) { // execute if match is found
       const maxRange = parseInt(plane[2]);
       const maxEconomy = parseInt(plane[3]);
       const maxBusiness = parseInt(plane[4]);
@@ -57,11 +57,11 @@ function details(airportsData, aeroplanesData, allFlightData) {
 
       // Handle too many seats being sold
       if (economySeats > maxEconomy) {
-        flightError = `Too many economy seats have been sold`;
+        flightError = `${economySeats-maxEconomy} too many economy seats have been sold (${economySeats}>${maxEconomy})`;
       } else if (businessSeats > maxBusiness) {
-        flightError = `Too many business seats have been sold`;
+        flightError = `${businessSeats-maxBusiness} too many business seats have been sold (${businessSeats}>${maxBusiness})`;
       } else if (firstSeats > maxFirst) {
-        flightError = `Too many first class seats have been sold`;
+        flightError = `${firstSeats-maxFirst} too many first class seats have been sold (${firstSeats}>${maxFirst})`;
       }
 
       // Handle flight range error
@@ -74,17 +74,17 @@ function details(airportsData, aeroplanesData, allFlightData) {
         }
 
         if (distance > maxRange) {
-          flightError = `A ${aircraftType} doesn't have the range for this flight`;
+          flightError = `A ${aircraftType.toLowerCase()} aircraft doesn't have the range for this flight`;
         }
       }
 
-      // Calculate profit/loss if no errors so far
+      // Calculate profit/loss if no errors so far (used previous code)
       if (!flightError) { 
         const runningCostPerSeatPer100km = parseFloat(plane[1].replace('£', ''));
-        const allSeats = parseInt(economySeats) + parseInt(businessSeats) + parseInt(firstSeats); // Convert seats to numbers
-        const totalCost = (runningCostPerSeatPer100km * distance * allSeats) / 100;
+        const allSeats = economySeats + businessSeats + firstSeats; 
+        const totalCost = (runningCostPerSeatPer100km * distance * allSeats) / 100; // formula for cost of flight
         const profitLoss = totalRev - totalCost;
-        flightDetails.push(`This flight will be from ${departureAirport} to ${arrivalAirport} on a ${aircraftType} aircraft and has an expected profit/loss of £${profitLoss.toFixed(2)}`);
+        flightDetails.push(`This flight will be from ${departureAirport} to ${arrivalAirport} on a ${aircraftType.toLowerCase()} aircraft and has an expected profit of £${profitLoss.toFixed(2)}`);
       }
     }
 
@@ -93,6 +93,7 @@ function details(airportsData, aeroplanesData, allFlightData) {
       flightDetails.push(flightError);
     } 
   }
+  // writing to a txt file 
   const fs = require('fs')
     const fileDetails = flightDetails.join('\n');
     fs.writeFile('Output.txt', fileDetails, (err) => {
@@ -102,95 +103,3 @@ function details(airportsData, aeroplanesData, allFlightData) {
 }
 
 console.log(details (airportsData, aeroplaneData, allFlightData))
-
-/*
-function revenue(valid_flight_data){
-    const listRev = []
-    for(row of valid_flight_data){
-        //revenue from all economy, business and first class seats
-        const economy = row[3]*row[6] 
-        const business = row[4]*row[7] 
-        const first = row[5]*row[8] 
-        const total = economy+business+first
-        listRev.push(total.toFixed(2)) //rounds to 2 decimal places 
-    }
-    return listRev
-}
-
-function distance (airportsData, valid_flight_data){
-    const distances = []
-    for (flight of valid_flight_data) {
-        const departureAirport = flight[0]; 
-        const arrivalAirport = flight[1];
-        // finds a match between index 0 in airports csv and arrival airport in valid_flight_data
-        const airportData = airportsData.find(airport => airport[0] === arrivalAirport); 
-        if (airportData) { //will execute if a match is found
-            if (departureAirport === 'MAN') {
-              distances.push(airportData[2]); // gets distance from MAN
-            } else if (departureAirport === 'LGW') {
-              distances.push(airportData[3]); // get distance from LGW
-            }
-        }
-    }
-    return distances
-}
-
-function cost(aeroplaneData, valid_flight_data) {
-    const costs = [];
-    for (let i = 0; i < valid_flight_data.length; i++) { 
-      const flight = valid_flight_data[i];
-      const aircraftType = flight[2]; // Get aircraftType from flight
-      const distances = distance(airportsData, valid_flight_data);
-      const flightDistance = distances[i]; 
-      // finds a match between index 0 in aeroplanes csv data and aircraft type in valid_flight_data
-      const aeroplane = aeroplaneData.find(plane => plane[0] === aircraftType);
-      if (aeroplane) { //will execute if a match is found
-        const runningCostPerSeatPer100km = parseFloat(aeroplane[1].replace('£', '')); //removes £ and makes it an integer
-        const economySeats = parseInt(flight[3]);
-        const businessSeats = parseInt(flight[4]);
-        const firstSeats = parseInt(flight[5]);
-        const allSeats = economySeats + businessSeats + firstSeats; 
-        const totalCost = (runningCostPerSeatPer100km * flightDistance * allSeats) / 100; //calculation for total cost of flight
-        costs.push(totalCost.toFixed(2)); 
-      } 
-    }
-    return costs;
-  }
-
-function profitLoss(){
-    const revenues = revenue(valid_flight_data); 
-    //console.log(revenues)
-    const costs = cost(aeroplaneData, valid_flight_data); 
-    //console.log(costs)
-    const profits = [];
-    for (let i = 0; i < revenues.length; i++) { 
-        const totalProfit = revenues[i] - costs[i];
-        profits.push(totalProfit.toFixed(2));
-      }
-    return profits
-}
-
-function flightDetail (valid_flight_data){
-    const profits = profitLoss(); 
-    const details = []
-    for (let i = 1; i < valid_flight_data.length; i++){
-        details.push(`This flight will be from ${valid_flight_data[i][0]} to ${valid_flight_data[i][1]} on a ${valid_flight_data[i][2]} aircraft and has an expected profit of £${profits[i]}`)
-    }
-    //writes flight details to output.txt file
-    const fs = require('fs')
-    const fileDetails = details.join('\n');
-    fs.writeFile('Output.txt', fileDetails, (err) => {
-        if (err) throw err;
-    })
-    return details
-}
-    */
-
-
-
-//console.log(revenue(valid_flight_data)) //prints list of revenues for each valid flight
-//console.log(distance(airportsData, valid_flight_data)) //prints list of distances for each valid flight
-//console.log(cost(aeroplaneData, valid_flight_data)) //prints list of revenues for each valid flight
-//console.log(profitLoss()) //prints list of profits for each valid flight
-//console.log(flightDetail (valid_flight_data)) //prints list of details for each valid flight
-//console.log(error (invalid_flight_data, aeroplaneData))
